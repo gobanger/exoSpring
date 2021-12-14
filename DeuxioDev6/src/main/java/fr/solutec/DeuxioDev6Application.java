588485a9a09436f1ec4cf98a90bcd2b169f7f6e6
gobanger <80410106+gobanger@users.noreply.github.com>
2021-12-14 12:11:32 +0100

    Add files via upload

---
+++ DeuxioDev6/src/main/java/fr/solutec/DeuxioDev6Application.java
@@ -0,0 +1,13 @@
+package fr.solutec;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeuxioDev6Application{
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeuxioDev6Application.class, args);
+	}
+
+}
